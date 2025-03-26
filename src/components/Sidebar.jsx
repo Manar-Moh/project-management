3daf50820f063d796839fd9d0586e04f0b5ae432
Manar-Moh <manor.moh6@gmail.com>
2025-03-27 00:00:51 +0200

    init

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+export function Sidebar() {
+  return (
+    <div className="w-1/5 bg-gray-800 h-screen">
+      <h1 className="text-white text-2xl font-bold p-4">Sidebar</h1>
+      <ul>
+        <li className="text-white p-4">Link 1</li>
+        <li className="text-white p-4">Link 2</li>
+        <li className="text-white p-4">Link 3</li>
+      </ul>
+    </div>
+  );
+}
