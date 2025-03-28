611a430353476b04963313a5e37ed24d97bac068
Manar-Moh <manor.moh6@gmail.com>
2025-03-28 22:42:28 +0200

    handle on select project

--- src/components/sidebar/ProjectLink.jsx
+++ src/components/sidebar/ProjectLink.jsx
@@ -1,9 +1,10 @@
-export function ProjectLink({ title, active }) {
+export function ProjectLink({ title, active,onClick }) {
   return (
     <li
       className={`text-white p-2 hover:bg-orange-600 hover:translate-x-5 hover:rounded-md  ${
         active ? "bg-orange-600 translate-x-5 rounded-md" : ""
       }`}
+      onClick={onClick}
     >
       {title}
     </li>
